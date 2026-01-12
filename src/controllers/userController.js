871fabd657604559474f42a9d21f5e5ca1230a23
Rosario Garufi <saro.garufi1@gmail.com>
2026-01-12 18:01:11 +0100

    Finish functional api of the project

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,9 @@ const asyncHandler = require("express-async-handler")
 const {StatusCodes} = require("http-status-codes");
 const generateToken = require("../utils/generateToken");
 const uploadToCloudinary = require("../utils/cloudinaryUpdate");
+const Song = require("../models/Song");
+const Artist = require("../models/Artist");
+const Playlist = require("../models/Playlist");
 
 
 //!desc - createUser
@@ -119,9 +122,146 @@ const updateUserProfile = asyncHandler(async(req, res) => {
 
 })
 
+//!desc - liked song
+//methos PUT - private
+//route /api/v1/users
+
+const likedSong = asyncHandler(async(req,res) => {
+    const {songId} = req.body;
+    //check if the song exist
+    const song = await Song.findById(songId);
+    if(!song){
+        res.status(StatusCodes.NOT_FOUND);
+        throw new Error("Song not exist");
+    }
+
+    //find the user
+    const user = await User.findById(req.user._id);
+    if(!user){
+        res.status(StatusCodes.NOT_FOUND);
+        throw new Error("User not found!");
+    }
+
+    if(user.likedSongs.some((id) => id.equals(songId))){
+        user.likedSongs = user.likedSongs.filter((id) => !id.equals(songId));
+    }else{
+        user.likedSongs.push(songId)
+    }
+    await user.save()
+    res.status(StatusCodes.OK).json(user)
+})
+
+//!desc - Follow Artist
+//methos PUT - private
+//route /api/v1/users
+
+const followArtist = asyncHandler(async(req,res) => {
+    const  {artistId} = req.body;
+    const userId = req.user._id
+    //check if the artist exist
+    const artist = await Artist.findById(artistId);
+    if(!artist){
+        res.status(StatusCodes.NOT_FOUND);
+        throw new Error("The artist not exist");
+    }
+
+    //find the user
+    const user = await User.findById(userId);
+    if(!user){
+        res.status(StatusCodes.NOT_FOUND);
+        throw new Error("User not found!")
+    }
+
+    const isFollowing = user.followedArtist.some((id) => id.equals(artistId));
+
+    if(!isFollowing){
+        //follow artist
+        await Promise.all([
+            User.updateOne(
+                {_id: userId},
+                {$addToSet: {followedArtist: artistId}}
+            ),
+            Artist.updateOne(
+                {_id: artistId},
+                {$inc: {followers: 1}}
+            )
+        ])
+    }else{
+        //UNFOLLOW
+        await Promise.all([
+            User.updateOne(
+                {_id: userId},
+                {$pull: {followedArtist: artistId}}
+            ),
+            Artist.updateOne(
+                {_id: artistId},
+                {$inc: {followers: -1}}
+            )
+        ])
+    }
+
+    res.status(StatusCodes.OK).json({
+        message: isFollowing ? "Artist Unfollowed" : "Artist Followed"
+    })
+})
+
+
+//!desc - Follow Playlist
+//methos PUT - private
+//route /api/v1/users
+
+const followPlaylist = asyncHandler(async(req, res) => {
+    const {playlistId} = req.body;
+    const userId = req.user._id
+
+    //check if the user exist
+    const user = await User.findById(userId)
+      if(!user){
+        res.status(StatusCodes.NOT_FOUND);
+        throw new Error("User not found");
+    }
+
+    
+    //check is the playlist exist
+    const playlist = await Playlist.findById(playlistId);
+    if(!playlistId){
+        res.status(StatusCodes.NOT_FOUND);
+        throw new Error("Playlist not found");
+    }
+
+    const isFollowing = user.followedPlaylist.some((id) => id.equals(playlistId));
+
+    if(!isFollowing){
+        await Promise.all([
+            user.updateOne(
+                {$addToSet: {followedPlaylist: playlistId}}
+            ),
+            playlist.updateOne(
+                {$inc: {followers: 1}}
+            )
+        ])
+    }else{
+        await Promise.all([
+            user.updateOne(
+                {$pull : {followedPlaylist: playlistId}}
+            ),
+            playlist.updateOne(
+                {$inc: {followers: -1}}
+            )
+        ])
+    }
+
+    res.status(StatusCodes.OK).json({
+        message: isFollowing ? "Unfollow the playlist" : "Follow the playlist"
+    })
+
+})
 module.exports = {
     registerUser,
     loginUser,
     getUserProfile,
-    updateUserProfile
+    updateUserProfile,
+    likedSong,
+    followArtist,
+    followPlaylist
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser, getUserProfile, updateUserProfile } = require("../controllers/userController");
+const { registerUser, loginUser, getUserProfile, updateUserProfile, likedSong, followArtist, followPlaylist } = require("../controllers/userController");
 const { protectRoute } = require("../middlewares/authMiddleware");
 const upload = require("../middlewares/upload");
  
@@ -12,6 +12,10 @@ userRouter.post("/login", loginUser)
 
 //private
 userRouter.get("/profile", protectRoute, getUserProfile)
+userRouter.put("/liked-song", protectRoute, likedSong)
+userRouter.put("/follow-artist", protectRoute, followArtist)
+userRouter.put("/follow-playlist" ,protectRoute, followPlaylist)
+
 userRouter.put("/profile", upload.single("profilePicture"), protectRoute, updateUserProfile)
 
 
